3d199a021c8829d24ca19fdcf0235d47ad061afb
Анастасия Ничипорчук <avnichi1@mts.ru>
2021-07-22 23:57:21 +0300

    docker finish

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 const DIST_PATH = '/dist';
 
 app.use(express.static(`${__dirname}${DIST_PATH}`));
@@ -10,6 +10,6 @@ app.get('/*', (req, res) => {
   res.sendFile(`${__dirname}${DIST_PATH}/index.html`);
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });
