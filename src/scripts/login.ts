42313bd16f6838616b2cd0f5a0d1e16d2077d4eb
Angry Fennec <shellfix@gmail.com>
2021-07-22 20:54:06 +0300

    husky?

--- src/scripts/login.ts
+++ src/scripts/login.ts
@@ -4,5 +4,4 @@ export function initLogin(element): void {
   const $loginForm: HTMLFormElement = element.querySelector('#auth-form');
   setFormSubmitHandler($loginForm, '/chat', 'signin');
   setFormValidationHandler($loginForm);
-  console.log('xcv');
 }
